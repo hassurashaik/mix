1008455e6d411f21103e3eeeb4b77dc44c2bee08
hassurashaik <130497782+hassurashaik@users.noreply.github.com>
2026-02-06 10:27:45 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -17,7 +17,7 @@ FS_ORIG = 16000
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--wsj0_path",
-    default=r"C:\Users\Shaik\Documents\temp_conv\pywsj0-mix",
+    default=r"/content/mix/wsj0-mix/2speakers/wav8k/min"
     help="Path to WSJ0 root"
 )
 parser.add_argument("--output_folder", default="wsj0-mix")
